24ddbb1a3a34a36c1db6599bf02c40388853e2b9
liushuxin <shuxinliu2016@gmail.com>
2019-03-13 10:44:31 +0800

    fix entry js

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class App extends React.Component{
-constructor(props){
-  this.state = {}
-}
-render(){
-return <div>渲染面板</div>
-}
+class App extends React.Component<any,any>{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return <div>渲染面板</div>
+  }
 
 }
-export default App;
+
+ReactDOM.render(<App/>,
+  document.querySelectorAll('#app')[0])
 
