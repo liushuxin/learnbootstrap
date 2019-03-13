82fdaefe5d2cb01b5d51525c1baf2c8f595e807c
liushuxin <shuxinliu2016@gmail.com>
2019-03-13 10:26:12 +0800

    init src

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class App extends React.Component{
+constructor(props){
+  this.state = {}
+}
+render(){
+return <div>渲染面板</div>
+}
+
+}
+export default App;
+
