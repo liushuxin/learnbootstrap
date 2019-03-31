1ba998b6de61a9ad3622181f5ce65daef30d1e74
liushuxin <shuxinliu2016@gmail.com>
2019-03-31 14:45:43 +0800

    add less

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const Koa = require('koa');
-const app = new Koa();
+const route = require('koa-router');
+const serve = require('koa-static');
+const path = require('path');
 
+const app = new Koa();
+// 1.主页静态网页 把静态页统一放到public中管理
+const home = serve(path.join(__dirname) + '/public/');
+// 3.分配路由
+app.use(home); 
 app.use(async ctx => {
     ctx.body = 'Hello World';
 });
+
+//app.use(route.get('/', hello));
 app.on('error', err => {
     log.error('server error', err)
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.less';
 class App extends React.Component<any,any>{
-  constructor(props){
+  constructor(props:any){
     super(props);
   }
   render(){
-    return <div>渲染面板</div>
+    return <div className="lanxin-home-wrapper">
+      <div className="lanxin-header-wrapper">
+        兰新科技
+      </div>
+      <div className="lanxin-content-wrapper">
+渲染面板
+      </div>
+      
+    </div>
   }
 
 }
 
 ReactDOM.render(<App/>,
-  document.querySelectorAll('#app')[0])
+  document.getElementById('app')
+ );
 
