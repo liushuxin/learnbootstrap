6b3cda5f50f4493ff70470f8cdd625de1abd8acf
liushuxin <shuxinliu2016@gmail.com>
2019-03-31 17:22:41 +0800

    add info show

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,11 @@ class App extends React.Component<any,any>{
   render(){
     return <div className="lanxin-home-wrapper">
       <div className="lanxin-header-wrapper">
-        <span className="header-title">兰新科技</span>
+        <span className="header-title">兰新平台</span>
         
       </div>
       <div className="lanxin-content-wrapper">
-渲染面板
+            渲染面板
       </div>
       
     </div>
