bea6c9b89a827147673a177767966263015dad77
liushuxin <shuxinliu2016@gmail.com>
2019-06-21 11:54:07 +0800

    fix menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
 import ItemMenu from './itemMenu';
-const { useState } = React;
+const { useState, useEffect, useLayoutEffect, useContext,useReducer } = React;
+function reducer () {
 
+}
 const Menu = (props: any) => {
-    const [state, setState] = useState({ data: 'React Hooks' });
+    const initialState = {count:0};
+    const [state, setState] = useState({ data: 'red', });
+    // const context = useContext(Context);
+    let lightArr = ['red', 'blue', 'yellow'];
+    let i = 1;
+    useEffect(
+        () => {
+          
+            setInterval(() => {
+                console.log("异步操作！！！");
+                
+                setState({
+                    data: lightArr[i]
+                });
+                i = i + 1;
+                if (i === 3) {
+                    i = 0;
+                }
+
+            }, 3000);
+            
+            
+
+    },[]);
     return <div>
         菜单：
        <ItemMenu 
--- src/components/menu/itemMenu.tsx
+++ src/components/menu/itemMenu.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import getThemeContext from '../themeContext';
 const ThemeContext = getThemeContext();
+const { useContext } = React;
 class ItemMenu extends React.Component<any,any> {
     static contextType = ThemeContext;
     constructor(props:any){
@@ -9,20 +10,28 @@ class ItemMenu extends React.Component<any,any> {
     }
     componentDidMount(){
         const { setState} = this.props;
-        setTimeout(()=> {
-            setState({
-                data:'我改变啦',
-            })
+        // setTimeout(()=> {
+        //     setState({
+        //         data:'blue',
+        //     })
 
-        },3000);
+        // },3000);
     }
     render(){
         const {data:{data}} = this.props;
+       // const value = useContext(ThemeContext);
         return <div>
             <div>
                 Context 外部信息：{this.context}
             </div>
-            主题颜色：{data}
+            <svg width="100%" height="100%" version="1.1"
+                xmlns="http://www.w3.org/2000/svg">
+
+                <circle cx="100" cy="50" r="40" stroke={data}
+                    strokeWidth="2" fill={data} />
+
+            </svg>
+            
             </div>
     }
 }
