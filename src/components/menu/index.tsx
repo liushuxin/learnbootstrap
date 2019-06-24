0c501d4a380daa8bbc04e2c53e2bd47261fcef39
liushuxin <shuxinliu2016@gmail.com>
2019-06-24 16:53:52 +0800

    fix menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,12 +1,31 @@
 import * as React from 'react';
 import ItemMenu from './itemMenu';
 const { useState, useEffect, useLayoutEffect, useContext,useReducer } = React;
-function reducer () {
-
+/**
+ * 
+ * @param 
+ * @param asd 
+ */
+const initialState = { count: 0 };
+function reducer(state:any, action:any) {
+    switch (action.type) {
+        case 'reset':
+            return initialState;
+        case 'increment':
+            return { count: state.count + 1 };
+        case 'decrement':
+            return { count: state.count - 1 };
+        default:
+            // A reducer must always return a valid state.
+            // Alternatively you can throw an error if an invalid action is dispatched.
+            return state;
+    }
 }
+
 const Menu = (props: any) => {
     const initialState = {count:0};
     const [state, setState] = useState({ data: 'red', });
+    const [state1, dispatch] = useReducer(reducer, { count: 3 });
     // const context = useContext(Context);
     let lightArr = ['red', 'blue', 'yellow'];
     let i = 1;
@@ -30,7 +49,17 @@ const Menu = (props: any) => {
 
     },[]);
     return <div>
-        菜单：
+        
+        菜单：{state1.count}
+        <button onClick={() => dispatch({ type: 'reset' })}>
+            Reset
+      </button>
+        <button onClick={() => dispatch({ type: 'increment' })}>
+            Add
+      </button>
+        <button onClick={() => dispatch({ type: 'decrement' })}>
+            Subtract
+      </button>
        <ItemMenu 
             setState={setState}
             data={state}
