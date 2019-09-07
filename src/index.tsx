323cb08362b1590d1b6d9a1f15d11c872ec79776
liushuxin <shuxinliu2016@gmail.com>
2019-09-07 14:09:58 +0800

    add lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,67 +1,63 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import './index.less';
-import { HashRouter as Router, Route, Link,Switch } from "react-router-dom";
-import AdminAdd from './page/admin/add';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.less";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+import AdminAdd from "./page/admin/add";
 //@ts-ignore
-import HomePage from './page/homePage';
+import HomePage from "./page/homePage";
 //@ts-ignore
-import Header from './components/Header';
+import Header from "./components/Header";
 const { Suspense, lazy } = React;
 //@ts-ignore
-const About = lazy(() => import(/* webpackChunkName: "About" */'./page/about'));
+const About = lazy(() =>
+  import(/* webpackChunkName: "About" */ "./page/about")
+);
 //@ts-ignore
-const Admin = lazy(() => import(/* webpackChunkName: "Admin" */'./page/admin'));
-class App extends React.Component<any,any>{
-  constructor(props:any){
+const Admin = lazy(() =>
+  import(/* webpackChunkName: "Admin" */ "./page/admin")
+);
+class App extends React.Component<any, any> {
+  constructor(props: any) {
     super(props);
   }
-  render(){
-    return <div className="lanxin-home-wrapper">
-       <Router>
-       <Suspense fallback={<div>Loading...</div>}>
-      
-    
-      <div className="lanxin-header-wrapper">
-       
-          <div className="header-nav">
-            <nav>
-            <Header></Header>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about/">About</Link>
-                </li>
-                <li>
-                  <Link to="/admin/">Users</Link>
-                </li>
-              </ul>
-            </nav>
-
-           
-          </div>
-       
-        
+  render() {
+    return (
+      <div className="lanxin-home-wrapper">
+        <Router>
+          <Suspense fallback={<div>Loading...</div>}>
+            <div className="lanxin-header-wrapper">
+              <div className="header-nav">
+                <nav>
+                  <Header></Header>
+                  <ul>
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/about/">About</Link>
+                    </li>
+                    <li>
+                      <Link to="/admin/">Users</Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+            <div className="lanxin-content-wrapper">
+              <Switch>
+                <Route path="/" exact component={HomePage} />
+                <Route path="/about/" component={About} />
+                <Route exact path="/admin" component={Admin} />
+                 <Route exact path="/admin/add/:id" component={AdminAdd}></Route>
+              </Switch>
+            </div>
+          </Suspense>
+        </Router>
       </div>
-      <div className="lanxin-content-wrapper">
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/about/" component={About} />
-        <Route exact  path="/admin" component={Admin}/>
-        <Route exact path="/admin/add/:id" component={AdminAdd}></Route>
-      </Switch>
-      </div>
-      </Suspense>
-      </Router>
-    </div>
+    );
   }
 }
-ReactDOM.render(<App/>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById("app"));
 /**
  * Context设计目的是为了共享那些对于一个组件树而言是“全局”的数据，例如当前认证的用户、主题或首选语言
  */
-
